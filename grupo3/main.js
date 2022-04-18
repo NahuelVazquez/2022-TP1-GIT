b5d28f351f93f69da480211d4ccdfe01b9b50f39
Diego Sosa <dhsosa98@gmail.com>
2022-04-17 23:21:35 -0300

    Sum two numbers

--- grupo3/main.js
+++ grupo3/main.js
@@ -1,4 +1,13 @@
+(function (){
+
 const label = prompt('Insert your label')
 
-document.getElementById('id-label').innerHTML += label ? label.toUpperCase() : 'NO LABEL'
+document.getElementById('render-label').innerHTML += label ? label.toUpperCase() : 'NO LABEL'
 
+const number = prompt('Insert your first number')
+
+const number2 = prompt('Insert your second number')
+
+document.getElementById('sum-label').innerHTML += `${number} + ${number2} = ${Number(number) + Number(number2)}`
+
+}())
