50451a0ba5052d7c83be9803c4fae2c7fa584eb1
Gowther <AcevedoAriel@outlook.com>
2022-04-17 21:56:28 -0300

    nomalizado de texto y cambio a textos

--- grupo3/main.js
+++ grupo3/main.js
@@ -1,3 +1,5 @@
-const label = prompt('Insert your label')
+const label = prompt("Ingresa algun label");
 
-document.getElementById('id-label').innerHTML += label
+document.getElementById("id-label").innerHTML += label
+  ? label.normalize()
+  : "No ingresaste nada bro";
