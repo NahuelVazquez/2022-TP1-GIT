71cde31390963844071005ec68c7c808c7385c72
gonza1020 <gonzalozoloaga44@gmail.com>
2022-04-19 11:12:30 -0300

    adding square function

--- Grupo6/nodeProject/app.js
+++ Grupo6/nodeProject/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const square  = require('./function')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let n = 3
+  const resp = `El cuadrado de ${n} es: ` + JSON.stringify(square(n))
+  res.send(resp)
 })
 
 app.listen(port, () => {
