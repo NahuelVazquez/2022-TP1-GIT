a0298df2f7b767e159755cb8b27638bb9ab092f2
Matias Acosta <acosta_matias8@hotmail.com>
2022-04-19 11:38:07 -0300

    adding function suma

--- Grupo6/nodeProject/app.js
+++ Grupo6/nodeProject/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const square  = require('./functions/square')
+const suma = require('./functions/suma')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.set("view engine","ejs")
 app.get('/', (req, res) => {
-  res.render("index",{square: square(3)})
-
+  res.render("index",{square: square(3),suma: suma(2,2)})
 })
 
 app.listen(port, () => {
