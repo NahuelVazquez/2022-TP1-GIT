9fbae5346263392182800e61bf1e42aa3b036103
Matias <matiasmartincampuzano16@gmail.com>
2022-04-20 15:22:20 -0300

    Revert "cambiando hello world"
    This reverts commit 356a2d50514418381a2060d92dc980a57c1a5e75.

--- Grupo5/tp1.js
+++ Grupo5/tp1.js
@@ -5,7 +5,11 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    // Send the response body as "Hello World"
+<<<<<<< HEAD
    response.end('Hell World, Matias :)\n');
+=======
+   response.end('Hellow World, soy Lu :)\n');
+>>>>>>> parent of 356a2d5 (cambiando hello world)
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/%27');
